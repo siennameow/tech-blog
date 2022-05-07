7d202173130a9e263b7b7a926c0d5a1080038e1c
Hexuan Li <lihexuan1@gmail.com>
2022-05-07 15:50:26 -0700

    complete association of models

--- models/index.js
+++ models/index.js
@@ -5,16 +5,29 @@ const Comment = require('./Comment');
 
 // define association
 //User has many post
-
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+})
 //post belong to user
-
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+})
 //user has many comment
-
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+})
 //comment belong to user
-
+Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+})
 //post has many comment
-
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: 'casecade'
+})
 //comment belong to post
-
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+})
 //export models
 module.exports = { User, Post, Comment };
